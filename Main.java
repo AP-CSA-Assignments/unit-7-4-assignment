fc425fdf3cb0f22746a21549ec39f9cf76edcc39
Kyle Vu <ktvu@hbuhsd.edu>
2025-02-23 21:08:53 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,16 +5,32 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Feel free to test code here
+    Scanner sc = new Scanner(System.in);
+    ArrayList<String> input = new ArrayList<String>();
+
+    String word = "";
+    while (!word.equals("STOP"))
+    {
+      System.out.println("Implement me!");
+    }
   }
 
-  public static void shiftLeft(ArrayList<String> arr)
+  /** Returns the number of Strings in the ArrayList that has the letter
+   * as the second letter in that word.
+   * Precondition: The arr is not null, and is not empty.
+   * Precondition: The letter is exactly one letter.
+   */
+  public static int countSecondInitial(ArrayList<String> arr, String letter)
   {
-    
+    return 0;
   }
 
-  public static printStatistics(final ArrayList<Integer> arr)
+  /** Returns the index of the second occurrence of target within the ArrayList
+   * If the target is not found, or is only found once, then -1 is returned.
+   * Precondition: Neither arr nor target are null.  The arr is non-empty.
+   */
+  public static int searchSecond(final ArrayList<String> arr, String target)
   {
-    
+    return -1;
   }
 }
